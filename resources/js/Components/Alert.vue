2b442614bb9defb3cd6d6b04e589bbb8f8bc8bfd
Md.Miraj Khan <miraj.net.xp@gmail.com>
2023-12-13 11:53:26 +0600

    Add transition

--- resources/js/Components/Alert.vue
+++ resources/js/Components/Alert.vue
@@ -1,24 +1,26 @@
 <template>
-    <div v-if="show" class="rounded-md bg-green-50 p-4">
-        <div class="flex">
-            <div class="flex-shrink-0">
-                <CheckCircleIcon class="h-5 w-5 text-green-400" aria-hidden="true"/>
-            </div>
-            <div class="ml-3">
-                <p class="text-sm font-medium text-green-800">{{ modelValue }}</p>
-            </div>
-            <div class="ml-auto pl-3">
-                <div class="-mx-1.5 -my-1.5">
-                    <button @click="onCloseAlert"
-                            type="button"
-                            class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
-                        <span class="sr-only">Dismiss</span>
-                        <XMarkIcon class="h-5 w-5" aria-hidden="true"/>
-                    </button>
+    <Transition name="fade">
+        <div v-if="show" class="rounded-md bg-green-50 p-4">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <CheckCircleIcon class="h-5 w-5 text-green-400" aria-hidden="true"/>
+                </div>
+                <div class="ml-3">
+                    <p class="text-sm font-medium text-green-800">{{ modelValue }}</p>
+                </div>
+                <div class="ml-auto pl-3">
+                    <div class="-mx-1.5 -my-1.5">
+                        <button @click="onCloseAlert"
+                                type="button"
+                                class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
+                            <span class="sr-only">Dismiss</span>
+                            <XMarkIcon class="h-5 w-5" aria-hidden="true"/>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </Transition>
 </template>
 
 <script setup>
@@ -35,6 +37,7 @@ const emit = defineEmits(['update:modelValue'])
 watch(() => props.modelValue, (message) => {
     if (message) {
         show.value = true
+        setTimeout(() => onCloseAlert(), 3000)
         return ''
     }
     show.value = false;
@@ -46,5 +49,14 @@ const onCloseAlert = function () {
 </script>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
 
 </style>
